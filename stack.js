01cd883c88711f20164d8a6619ab1b096e3b94ea
Đặng Văn Hậu <121596451+dev-snake@users.noreply.github.com>
2024-12-29 23:10:10 +0700

    add isValid method

--- stack.js
+++ stack.js
@@ -3,10 +3,10 @@ class Stack {
         this.items = []
     }
     push(value) {
-        this.items.push(value)
+        return this.items.push(value)
     }
     pop() {
-        this.items.pop()
+        return this.items.pop()
     }
     isEmpty() {
         return this.items.length === 0
@@ -18,10 +18,50 @@ class Stack {
         return this.items[this.items.length - 1]
     }
 }
+function isPalindrome(str) {
+    const stack = new Stack();
+    for (let char of str) {
+        stack.push(char)
+    }
+    for (let char of str) {
+        if (char !== stack.items.pop()) {
+            return false;
+        }
+    }
+    return true;
 
-const stack = new Stack()
-stack.push(1)
-stack.push(23)
-stack.push(11)
-stack.pop()
-console.log(stack.size());
+}
+function converToBinary(number) {
+    const stack = new Stack();
+    while (number > 0) {
+        stack.push(number % 2);
+        number = Math.floor(number / 2)
+    }
+    let binary = '';
+    while (!stack.isEmpty()) {
+        binary += stack.pop()
+    }
+    return binary;
+
+}
+function isValid(str) {
+    const stack = new Stack();
+    const matchingBrackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    for (let char of str) {
+        if (char === "{" || char === "(" || char === "[") {
+            stack.push(char)
+        } else if (char === "}" || char === ")" || char === "]") {
+            if (stack.pop() !== matchingBrackets[char]) {
+                return false;
+            }
+        }
+
+
+    }
+    return stack.isEmpty()
+}
+console.log(isValid('(){(())}'));
